1eb51b5188a4af9c6e152f70c7f43ce9173db4a1
ana clara <133878274+an4clara@users.noreply.github.com>
2024-12-13 15:39:47 -0400

    Update Servidor.py
    Implementando o método liberarVisitante para gerenciamento de acesso de visitante, incluindo tratamento de entradas inválidas e  mensagens claras para o usuário.

---
+++ IDControl/Servidor.py
@@ -0,0 +1,22 @@
+from Servidor import Servidor
+
+class Recepcionista (Servidor):
+  def __init__(self, nome, cpf, senha, matricula, departamento):
+    super().__init__(nome, cpf, senha, matricula, departamento)
+    def LiberarVisitante(self):
+      while True:
+        try:
+          liberar = input ("Deseja liberar visitante: SIM [1] NÃO [2]")
+          if liberar == 1:
+            print ("Acesso liberado.")
+            break
+          elif liberar == 2:
+            print ("Acesso negado.")
+            break
+          else:
+            print("Tente novamente!")
+        except SyntaxError:
+          print ("Entrada inválida! Digite 1 ou 2.")
+          
+      
+            
